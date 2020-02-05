052032da56b69b706f4c35ceabc92ca756acbe97
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-02-05 18:31:33 +0100

    Add context extension

---
+++ Database/MatchContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using MatchEntities.Enums;
+
+namespace Database
+{
+    public static class MatchContextExtensions
+    {
+        public static byte CurrentRank(this MatchContext dbContext, long playerId)
+        {
+            return dbContext.PlayerMatchStats
+                       .Where(x => x.SteamId == playerId && x.MatchStats.AvgRank != null && x.MatchStats.Source == Source.Valve)
+                       .OrderByDescending(x => x.MatchStats.MatchDate)
+                       .Select(x => (byte?) x.RankAfterMatch)
+                       .FirstOrDefault() ?? 0;
+        }
+    }
+}
