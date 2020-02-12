99f1b012f7913950123573777bf15f37be6a00f2
Kieron <kieron@mentor.gg>
2020-02-12 16:24:29 +0100

    Update CurrentRank() using enum

--- Database/MatchContextExtensions.cs
+++ Database/MatchContextExtensions.cs
@@ -7,13 +7,13 @@ namespace Database
 {
     public static class MatchContextExtensions
     {
-        public static byte CurrentRank(this MatchContext dbContext, long playerId)
+        public static MatchMakingRank CurrentRank(this MatchContext dbContext, long playerId)
         {
             return dbContext.PlayerMatchStats
                        .Where(x => x.SteamId == playerId && x.MatchStats.AvgRank != null && x.MatchStats.Source == Source.Valve)
                        .OrderByDescending(x => x.MatchStats.MatchDate)
-                       .Select(x => (byte?) x.RankAfterMatch)
-                       .FirstOrDefault() ?? 0;
+                       .Select(x => x.RankAfterMatch)
+                       .FirstOrDefault();
         }
     }
 }
