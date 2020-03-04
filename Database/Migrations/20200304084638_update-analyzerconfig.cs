a839a8fda33e0596fa3198a06a2f9dbb6296a0c7
Kieron <kieron@mentor.gg>
2020-03-04 09:47:24 +0100

    Add migration (FramesPerSecond int -> double)

---
+++ Database/Migrations/20200304084638_update-analyzerconfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class updateanalyzerconfig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Config_FramesPerSecond",
+                table: "MatchStats",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Config_FramesPerSecond",
+                table: "MatchStats",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldNullable: true);
+        }
+    }
+}
